6254ccadcb8bccbbc2e015a9f5eee6669690decf
Augustowilliamschutz <you@example.com>
2024-09-18 14:43:22 -0300

    projeto_inicial

---
+++ Nova pasta/logica-js-projeto_inicial/app.js
@@ -0,0 +1,17 @@
+//let titulo = document.querySelector('h1');
+//titulo.innerHTML =  'Jogo do numero secreto';
+
+//let paragrafo = document.querySelector ('p');
+//paragrafo.innerHTML =  'Escolha Um numero entre 1 e 10';
+
+function exibirTextoNaTela (tag){
+   let campo = document.querySelector(tag)
+   campo.innerHTML =  texto 
+   }
+   exibirTextoNaTela ('h1','Jogo do numero secreto')
+   exibirTextoNaTela ('p', 'Escolha Um numero entre 1 e 10' )
+   
+   
+   function verificarChute () {
+       console.log ('O botao foi clicado!')
+   }
